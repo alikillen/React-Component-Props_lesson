dbe0a80ec43ee85e834e3495ab9e332fac5f5cb1
alikillen <alisoneme1994@gmail.com>
2020-10-27 09:28:07 +1000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./styles/index.css"
+
+// const styles = {color: "blue", textAlign: "center"}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1 className="title">welcome to my react app</h1>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// class keyword will not work here for styling - need to use camelCase className
+
